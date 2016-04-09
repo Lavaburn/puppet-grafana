b56878421fe9c226f7a02ba3f1657601e7b3fc75
Nicolas Truyens <nicolas@truyens.com>
2016-04-09 16:55:59 +0200

    Changing /etc/puppet to /etc/grafana

--- lib/puppet/provider/grafana_rest.rb
+++ lib/puppet/provider/grafana_rest.rb
@@ -18,7 +18,7 @@ class Puppet::Provider::Rest < Puppet::Provider
   end
     
   def self.get_rest_info
-    config_file = "/etc/puppet/grafana_api.yaml"
+    config_file = "/etc/grafana/api.yaml"
     
     data = File.read(config_file) or raise "Could not read setting file #{config_file}"    
     yamldata = YAML.load(data)
